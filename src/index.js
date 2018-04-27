61141e9c8cd717ced2444bd76b27b412097dc370
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-04-27 14:35:30 +0100

    Add form within the app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import windowObjectLogs from "./js/window-object-logs";
-// the following can now be typed within the console:
-// store.getState()
-// store.subscribe(() => console.log('Look ma, Redux!!'))
-// store.dispatch( addArticle({ name: 'React Redux Tutorial for Beginners', id: 1 }) )
-// store.getState()
+/* 1) First part of the tutorial
 
+import windowObjectLogs from "./js/window-object-logs";
 import App from "./App";
+the following can now be typed within the console:
+store.getState()
+store.subscribe(() => console.log('Look ma, Redux!!'))
+store.dispatch( addArticle({ name: 'React Redux Tutorial for Beginners', id: 1 }) )
+store.getState()
+
+2) second part of the tutorial
+*/
+
+import provider from "./js/my-provider"
+import App from "./js/components/App";
 import style from "./main.css";
