f54db1f6376fb81ba1f5904f481dc0bf681872f1
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2018-04-27 10:41:03 +0100

    Copy initial state through webpack

---
+++ README.md
@@ -0,0 +1,3 @@
+:crystal_ball: React Redux for Beginners :crystal_ball:
+===
+This is the React Redux for Beginners tutorial branched from [Valentino Gagliardi's blog](https://www.valentinog.com/blog/react-redux-tutorial-beginners/)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <p>React Redux For Beginners</p>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("app"));
